6a7c090a220f723856ddf3e85339b14a5971b996
drssee <113401870+drssee@users.noreply.github.com>
2022-12-04 23:52:56 +0900

    22/12/04

---
+++ ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain2.java
@@ -0,0 +1,43 @@
+package jpabasic.ex1hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain2 {
+    public static void main(String[] args) {
+        //persistence->entitymanagerfactory->entitymanager
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //로직
+        try {
+            //jpql
+
+            //member객체들을 전부 가져와라 m <- 엔티티
+
+            //1번부터 10개 가져와라
+            //.setFirstResult(1)
+            //.setMaxResults(10)
+
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+            for (Member member : resultList) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/Member.java
@@ -0,0 +1,40 @@
+package jpabasic.ex1hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+//    @Column(name="id")
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
