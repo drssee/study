6f4dd8701397d7eac74425d8e7824bdae25c7995
drssee <sarmos88@naver.com>
2023-03-15 23:21:25 +0900

    자바 웹개발 워크북12

--- b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -38,6 +39,7 @@ public class BoardController {
     }
 
     @GetMapping("/register")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public void registerGet() {
 
     }
@@ -64,6 +66,7 @@ public class BoardController {
     }
 
     @GetMapping({"/read","/modify"})
+    @PreAuthorize("isAuthenticated()")
     public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
         BoardDTO boardDTO = boardService.readOne(bno);
         log.info(boardDTO);
--- b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -2,20 +2,32 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomUserDetailsService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     //스프링 시큐리티의 핵심
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("loadUserByUsername: "+username);
-        return null;
+
+        return User.builder().username("user1")
+                .password(passwordEncoder.encode("1111"))
+                .authorities("ROLE_USER")
+                .build();
     }
 }
