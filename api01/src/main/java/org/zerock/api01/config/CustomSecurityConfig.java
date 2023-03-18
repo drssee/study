2f3b3ed4a07b1f020f3640fd593810393fbdd8f9
drssee <sarmos88@naver.com>
2023-03-18 23:55:20 +0900

    자바 웹개발 워크북16

---
+++ api01/src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
@@ -0,0 +1,49 @@
+package org.zerock.api01.config;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomSecurityConfig.class);
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("-----web configure-----");
+        //함수형 인터페이스 구현체 리턴(매개변수 타입 WebSecurity webSecurity)
+        return (webSecurity) -> webSecurity.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        log.info("-----configure-----");
+        http.csrf().disable(); //csrf 토큰의 비활성화
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //세션 사용하지 않음
+        return http.build();
+    }
+}
---
+++ api01/src/main/java/org/zerock/api01/config/CustomServletConfig.java
@@ -0,0 +1,18 @@
+package org.zerock.api01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig implements WebMvcConfigurer {
+
+    //스프링 템플릿을 사용하지 않아 정적 파일의 경로를 수동등록 해줘야함
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
